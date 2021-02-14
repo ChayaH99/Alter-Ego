619a21a309fd4a33daa55734cdc69f498674006c
ChayaH99 <77052198+ChayaH99@users.noreply.github.com>
2021-02-14 11:39:56 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package alterego
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.gstatic.com"> 
+	<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Cardo:ital@0;1&family=Tenor+Sans&display=swap|Archivo" rel="stylesheet">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'alterego' ); ?></a>
+	
+
+	
+
+	<header id="masthead" class="site-header flex" style="<?php category_header_background(); ?> ">
+		<a href= "<?php get_site_url() ?>" class="absolute top-0 left-0 logo">
+		<img src= "<?php echo get_template_directory_uri() .
+		'/images/alter-ego-logo.svg' ; ?> " class="db">
+       
+	<nav id="site-navigation" class="main-navigation flex flex-auto items-end relative">
+			
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+					'menu_class'	=>  'main-menu ma0 pa0 list'
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+
+	
+		<div class="site-branding">
+		<?php echo wp_get_attachment_image(74,'full')?>
+		</div><!-- .site-branding -->
+
+		
+	</header><!-- #masthead -->
